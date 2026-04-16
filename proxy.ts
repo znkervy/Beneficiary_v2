30ab539413ff083d7a58b63c58bda9a345970f70
ArjelAaran <arjelaaran.deocampo.cics@ust.edu.ph>
2026-04-16 23:02:49 +0800

    added backend on login and signup

--- proxy.ts
+++ proxy.ts
@@ -7,5 +7,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
 };
